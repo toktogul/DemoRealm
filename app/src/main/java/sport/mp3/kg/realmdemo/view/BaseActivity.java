a408d2ef6a144dcc1777311a12c044c9e735e70a
Toktogul <toktogul31@yandex.ru>
2017-01-30 00:34:56 +0600

    edit BaseActivity

--- app/src/main/java/sport/mp3/kg/realmdemo/view/BaseActivity.java
+++ app/src/main/java/sport/mp3/kg/realmdemo/view/BaseActivity.java
@@ -16,17 +16,11 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected Presenter presenter;
     protected RecyclerView recyclerView;
     protected CarAdapter carAdapter;
-    private char[] alphabetS = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-    public static final String TAG = BaseActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        CesarCipher test = new CesarCipher();
-//        test.setAlphabet(alphabetS);
-//        test.setShift(3);
-//        Log.d(TAG, "onCreate: " + test.encode("The quick brown fox jumps over the lazy dog"));
         init();
     }
 
@@ -47,51 +41,4 @@ public abstract class BaseActivity extends AppCompatActivity {
     }
 
 
-}
-
-
-class CesarCipher {
-
-
-    private char[] alphabet;
-    private int shift;
-
-    public void setAlphabet(char[] alphabet) {
-        this.alphabet = alphabet;
-    }
-
-    public void setShift(int shift) {
-        this.shift = shift;
-    }
-
-    public String encode(String plainText) {
-        // реализация
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for (int i = 0; i < plainText.length(); i++) {
-            boolean isFind = false;
-
-            for (int k = 0; k < alphabet.length; k++) {
-                if (plainText.charAt(i) == alphabet[k]) {
-                    isFind = true;
-                    int lastKey = alphabet.length;
-                    int currentKey = k + shift;
-
-                    if (currentKey < lastKey)
-                        stringBuilder.append(alphabet[currentKey]);
-                    else
-                        stringBuilder.append(alphabet[currentKey - lastKey]);
-
-                    break;
-                }
-            }
-
-            if (!isFind) {
-                stringBuilder.append(plainText.charAt(i));
-            }
-        }
-
-
-        return stringBuilder.toString();
-    }
 }
