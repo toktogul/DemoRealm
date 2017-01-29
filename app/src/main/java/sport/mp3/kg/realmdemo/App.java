e9f1878339163a8486569f073ea7da4f8fcd93cc
Toktogul <toktogul31@yandex.ru>
2017-01-29 23:51:45 +0600

    test commit

--- app/src/main/java/sport/mp3/kg/realmdemo/App.java
+++ app/src/main/java/sport/mp3/kg/realmdemo/App.java
@@ -13,7 +13,9 @@ public class App  extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
-        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder(this).build();
-        Realm.setDefaultConfiguration(realmConfiguration);
+        RealmConfiguration realmConfiguration =
+                new RealmConfiguration.Builder(this).build();
+        Realm
+                .setDefaultConfiguration(realmConfiguration);
     }
 }
